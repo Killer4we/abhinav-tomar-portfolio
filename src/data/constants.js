0a9701033c8c8f78b137060adb565fb02cfd9436
Abhinav <abhinavajay20@gmail.com>
2023-12-26 19:13:55 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,262 @@
+export const Bio = {
+  name: "Abhinav Tomar",
+  roles: [
+    "Front End Developer",
+    "Algorithm Enthusiast",
+    "Competitive Programmer",
+    "Data Science Enthusiast"
+  ],
+  description:
+    "I am an undergraduate student at VIT, Chennai passionate about Competitive Programming, Data Science and Web Development. Dedicated to expanding knowledge and skills, thriving on challenges and seeking new opportunities.Relentlessly curious, embracing the evolving world of technology.",
+  github: "https://github.com/Killer4we",
+  resume:
+    "https://drive.google.com/file/d/1DHTt8dIG0tXy19HIWvvZYTiXWIRutpZj/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/abhinav-tomar-4bb88221b/",
+  leetcode: "https://leetcode.com/Abhinav_Tomar/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML5",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS3",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Wix",
+        image:
+          "https://i.pcmag.com/imagery/reviews/00Z1mnZCcGR9r9D5hNbsFbW-36.fit_lim.size_1050x591.v1612863800.jpg",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+    ],
+  },
+  {
+    title: "Technical",
+    skills: [
+      {
+        name: "C",
+        image: "https://i.pinimg.com/originals/6e/46/e7/6e46e7dbe2bb73dacc055e5dbd85c3ad.png",
+      },
+      {
+        name: "C++",
+        image:
+          "https://thumbs.dreamstime.com/b/c-programming-computer-language-logo-251853143.jpg",
+      },
+      {
+        name: "Java",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      
+    ],
+  },
+  {
+    title: "Academic",
+    skills: [
+      {
+        name: "Operating Systems",
+        image:
+          "https://cdn.pixabay.com/photo/2013/03/30/00/09/operating-system-97851_640.png",
+      },
+      {
+        name: "DSA",
+        image:
+          "https://i0.wp.com/esyncalliance.org/wp-content/uploads/2020/05/DSA.png?fit=425%2C300&ssl=1",
+      },
+      {
+        name: "Computer Networks",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQctrjDMrp5ddmAExKFsCI4hwrvBiupizFCsamk6rdpF8U0z_xn0zwnBPKrD6JviJBg5lc&usqp=CAU",
+      },
+      {
+        name: "DBMS",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc6tTOAHHe6jvgyWUqLEku7yKOZfIaBLQl7g&usqp=CAU",
+      },
+      {
+        name: "DAA",
+        image:
+          "https://static.vecteezy.com/system/resources/previews/007/202/443/non_2x/daa-letter-logo-design-on-black-background-daa-creative-initials-letter-logo-concept-daa-letter-design-vector.jpg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Vercel",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi5uvabTLM_b4nvD5dGj7rXwyn0NDAXVGdn9zXdsIB34JbWfDbZbzziyOS1tcC5su0Ys4&usqp=CAU",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C510BAQFKVHWrZnU_Qg/company-logo_200_200/0/1630573317820?e=2147483647&v=beta&t=50hYMwHKKEkzhNW4Nt3z89SqtfkErGPgS67y2iLo9KI",
+    role: "Culture Team Member",
+    company: "Fraternity of Young Innovators",
+    date: "Nov 2022 - Present",
+    desc: "Member of culture team of FYI. Helps in managing events",
+    skills: [
+      "Management",
+      "Team-Work",
+      "Problem Solving",
+      "Communication",
+    ],
+  },
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C560BAQH0jIUG8VWAmA/company-logo_100_100/0/1660488618930/ecellvitc_logo?e=1711584000&v=beta&t=Kya9VVPwFp_-AMFWiKaYTnraz-8m6gV235sD1HoCjmg",
+    role: "Project Representative",
+    company: "Business Innovation Community",
+    date: "Aug 2022 - Present",
+    desc: "As part of a collaborative team effort, I am contributing to the development and execution of VLX, a specialized platform modeled after OLX but exclusively tailored for college students.",
+    skills: [
+      "ReactJS",
+      "NodeJs",
+      "Tailwind CSS",
+      "MongoDB",
+      "HTML5",
+      "CSS3",
+      "JavaScript",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/C4D0BAQGeU2u797pjMQ/company-logo_100_100/0/1631313235717?e=1711584000&v=beta&t=Nj58cxDZiBIZClt5Gw7wUXg-TpWCVjSWgLu3sUzZBFo",
+    role: "Research Internship",
+    company: "ISRO-Indian Space Research Organization",
+    date: "Sep 2023 - Nov 2023",
+    desc: "Worked on Implementation of Key Chain Generation for Navigation Message Authentication.Worked with NavIC for the frequency band L5.Worked on NMA Generator and NMA Receiver.",
+    skills: [
+      "Matlab",
+      "C",
+      "C++",
+      "Excel",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://media.licdn.com/dms/image/D4D0BAQFrSCr90A7rQg/company-logo_100_100/0/1696871889807?e=1711584000&v=beta&t=gLyrC-h0RYpxQTH_czAt2_gwVIh8tFrGs0HX1M3Wq4I",
+    role: "Web Dev Intern",
+    company: "SwasthMind",
+    date: "Sep 2023 - Nov 2023",
+    desc: "Developed and deployed the website for SwasthMind using wix.",
+    skills: [
+      "Wix Website Builder",
+      "Wix Toolset",
+      "Wix",
+      "HTML",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C4D0BAQGgTYgPZiPkaA/company-logo_100_100/0/1631346260383?e=1711584000&v=beta&t=Sl1DkGMTwm8gdLle5FkYRjzqhrvIt4HN-O3vb4FR7As",
+    school: "Vellore Institute of Technology, Chennai",
+    date: "Oct 2021 - Sep 2025",
+    grade: "8.86 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Vellore Institute of Technology, Chennai. I have completed 5 semesters and have a CGPA of 8.86. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Bussiness Innovation Community Club (BIC) at VIT, where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://www.schooldekho.org/storage/logo//4ryqh5861ls0okksc4kow8wc48c8ow.jpg",
+    school: "Sant Gyaneshwar Model School, Alipur",
+    date: "Apr 2019 - Apr 2021",
+    grade: "94%",
+    desc: "I completed my class 12 high school education at Sant Gyaneshwar Model School, Alipur, where I studied Science with Computer Science.",
+    degree: "CBSE(XII), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://media.licdn.com/dms/image/C510BAQGt3zuy-Lj3lQ/company-logo_100_100/0/1630622634778/dlf_public_school_logo?e=1711584000&v=beta&t=LnAZNZpU-I2IF2IK4rXk0muqsiCWtNEzQoOMUBJGT7Q",
+    school: "DLF Public School, Ghaziabad",
+    date: "Apr 2019 - Apr 2019",
+    grade: "94%",
+    desc: "I completed my class 10 education at DLF Public School, Ghaziabad, where I studied Science with Computer Application.",
+    degree: "CBSE(X), Science with Computer",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
